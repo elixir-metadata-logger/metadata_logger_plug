98ecac0cb8533652733e3ccc156c24776eac5800
Chulki Lee <chulki.lee@gmail.com>
2018-08-09 09:53:30 -0700

    add hex pkg info and CHANGELOG.md

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule PlugMetadataLogger.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # hex
+      description: "Plug Middleware to log request and response into in metadata",
+      package: package()
     ]
   end
 
@@ -19,5 +23,17 @@ defmodule PlugMetadataLogger.MixProject do
 
   defp deps do
     [{:plug, "~> 1.5"}]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/chulkilee/plug_metadata_logger",
+        "Changelog" =>
+          "https://github.com/chulkilee/plug_metadata_logger/blob/master/CHANGELOG.md"
+      },
+      maintainers: ["Chulki Lee"]
+    ]
   end
 end
