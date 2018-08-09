051d72b30284983bd12737d68ce08b4792364781
Chulki Lee <chulki.lee@gmail.com>
2018-08-09 10:08:07 -0700

    add a test

--- test/plug_metadata_logger_test.exs
+++ test/plug_metadata_logger_test.exs
@@ -1,8 +1,52 @@
 defmodule PlugMetadataLoggerTest do
   use ExUnit.Case
+  use Plug.Test
+
+  import ExUnit.CaptureLog
+
   doctest PlugMetadataLogger
 
-  test "greets the world" do
-    assert PlugMetadataLogger.hello() == :world
+  defmodule MetadataFormatter do
+    def format(_level, _message, _timestamp, metadata) do
+      inspect(metadata) <> "\n"
+    end
+  end
+
+  setup do
+    on_exit(fn ->
+      :ok =
+        Logger.configure_backend(
+          :console,
+          format: nil,
+          device: :user,
+          level: nil,
+          metadata: :all,
+          colors: [enabled: false]
+        )
+    end)
+
+    Logger.configure_backend(:console,
+      format: {MetadataFormatter, :format},
+      colors: [enabled: false],
+      metadata: :all
+    )
+
+    :ok
+  end
+
+  test "metadata in logs" do
+    captured =
+      capture_log(fn ->
+        :get |> conn("/ping?foo=bar") |> PlugMetadataLogger.call([]) |> send_resp(200, "pong")
+      end)
+
+    assert captured =~ ~s(http_remote_ip: "127.0.0.1")
+    assert captured =~ ~s(http_method: "GET")
+    assert captured =~ ~s(http_path: "/ping")
+    assert captured =~ ~s(http_duration_ms: )
+    assert captured =~ ~s(http_status: 200)
+    assert captured =~ ~s(http_response_body_bytes: )
+
+    assert Logger.metadata() == []
   end
 end
