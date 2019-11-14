47f5403b961967dc6b0191ade3811d754b42e936
Chulki Lee <chulki.lee@gmail.com>
2019-11-14 15:40:25 -0800

    update dev/doc setup

--- mix.exs
+++ mix.exs
@@ -1,17 +1,22 @@
 defmodule MetadataLogger.Plug.MixProject do
   use Mix.Project
 
+  @version "0.1.1-dev"
+
   def project do
     [
       app: :metadata_logger_plug,
-      version: "0.1.1-dev",
+      version: @version,
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
       # hex
       description: "Plug Middleware to log request and response into in metadata",
-      package: package()
+      package: package(),
+
+      # ex_doc
+      docs: docs()
     ]
   end
 
@@ -24,6 +29,7 @@ defmodule MetadataLogger.Plug.MixProject do
   defp deps do
     [
       {:plug, "~> 1.5"},
+      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
   end
@@ -37,6 +43,15 @@ defmodule MetadataLogger.Plug.MixProject do
           "https://github.com/elixir-metadata-logger/metadata_logger_plug/blob/master/CHANGELOG.md"
       },
       maintainers: ["Chulki Lee"]
+    ]
+  end
+
+  defp docs do
+    [
+      name: "MetadataLogger.Plug",
+      source_ref: "v#{@version}",
+      canonical: "https://hexdocs.pm/metadata_logger_plug",
+      source_url: "https://github.com/elixir-metadata-logger/metadata_logger_plug"
     ]
   end
 end
